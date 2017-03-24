61b4d52fd72ca1f31ebb3c57af27a41394e1e5ce
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2017-03-24 16:48:27 +0000

    Refactoring

--- matrix_ctrl.py
+++ matrix_ctrl.py
@@ -1,27 +1,39 @@
-from sense_hat import ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 import time
+import os
 
-class PatternInputController():
+class MatrixController():
 
 	WHITE = [255,255,255]
 	BLACK = [0,0,0]
 	RED = [255,0,0]
-	
-	def __init__(self, sense):
+	USER_EXIT = 'exit'	
+
+	def __init__(self):
 		self.trying = False
-		self.sense = sense
+		self.sense = SenseHat()
 		self.handler = { "up": self.pushed_up,
 			 	 "down": self.pushed_down,
 				 "right": self.pushed_right,
 				 "left": self.pushed_left,
 				 "middle": self.pushed }
+
+	def get_username(self):
+		os.system("clear")
+		self.sense.clear()
+		print "Enter your Username. Type 'exit' to close the app \n"
+		return raw_input("Username: ")
 		
 	def get_input_pattern(self):
+		print "\nEnter your authentication pattern in the Raspberry Pi"
+		print "\n	- Move the joystick to move around"
+		print "\n	- Press the joystick to select/deselect"
+		print "\n	- Select any spot in the green lines to submit"
 		self.trying = True
 		self.sense.load_image("res/start_pattern.png")
 		self.x, self.y = [3, 3]
 		self.next_x, self.next_y = [3, 3]
-		self.hold_color = PatternInputController.BLACK
+		self.hold_color = MatrixController.BLACK
 		while self.trying:
 			event = self.sense.stick.wait_for_event(emptybuffer=True)
 			self.handler[event.direction](event)
@@ -31,12 +43,43 @@ class PatternInputController():
 				del pattern_entered[56:] # Delete last row
 				del pattern_entered[7::8] # Delete last coloumn
 				for i in range(len(pattern_entered)):
-					pattern_entered[i] = pattern_entered[i] != PatternInputController.BLACK
+					pattern_entered[i] = pattern_entered[i] != MatrixController.BLACK
 		self.sense.clear()
 		return pattern_entered
 
+	def exit(self):
+		print "\nBye!!"
+		self.sense.show_message("Bye!!")
+
+	def display_error(self, error):
+		self.sense.load_image("res/error.png")
+		print "\nAn error has occured: \n"
+		print error
+		print "\nPress Enter to continue"
+		raw_input()
+		self.sense.clear()
+
+	def display_success(self, user):
+		self.sense.load_image("res/success.png")
+		print "\n\n Welcome " + user['name'] + "!"
+		print "\n We will send you an email to <" + user['mail'] + "> every " + str(user['wait']) + " seconds with the weather data."
+		print "\n Press Ctrl + C to logout"
+		time.sleep(1)
+		self.sense.clear() 
+	
+	def animation(self):
+		"One second animation"
+		self.sense.load_image("res/cloud1.png")
+		time.sleep(0.25)
+		self.sense.load_image("res/cloud2.png")
+		time.sleep(0.25)
+		self.sense.load_image("res/cloud3.png")
+		time.sleep(0.25)
+		self.sense.load_image("res/cloud4.png")
+		time.sleep(0.25)
+
 	def clamp(self,value, min_value=0, max_value=7):
-		"check it doesn't go out of the boundaries of the matrix"
+		"Check it doesn't go out of the boundaries of the matrix"
 		return min(max_value, max(min_value, value))
 
 	def pushed_up(self,event):
@@ -64,10 +107,10 @@ class PatternInputController():
 
 	def toggle_color(self):
 		"If the joystick is pressed, change the color from Black to Red or viceversa"
-		if self.hold_color == PatternInputController.BLACK:
-			self.hold_color = PatternInputController.RED
+		if self.hold_color == MatrixController.BLACK:
+			self.hold_color = MatrixController.RED
 		else:
-			self.hold_color = PatternInputController.BLACK
+			self.hold_color = MatrixController.BLACK
 
 	def refresh(self):
 		#Color the current pixel with the proper color before moving
@@ -75,6 +118,6 @@ class PatternInputController():
 		#Saves the color of the next pixel
 		self.hold_color = self.sense.get_pixel(self.next_x,self.next_y)
 		#Moves to the next pixel, coloring it white
-		self.sense.set_pixel(self.next_x,self.next_y,PatternInputController.WHITE)
+		self.sense.set_pixel(self.next_x,self.next_y,MatrixController.WHITE)
 		#Updates the current position
 		self.x, self.y = self.next_x, self.next_y
---
+++ persistence.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+class Persistence():
+	
+	def __init__(self):
+		self.config = {
+			'user': 'root',
+			'password': 'p@ssword',
+			'host': '127.0.0.1',
+			'database': 'weather_app',
+			'raise_on_warnings': True
+		}
+
+	def connect(self):
+		try:
+			self.cnx = mysql.connector.connect(**self.config) 
+		except mysql.connector.Error as err:
+			if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+				print("Something is wrong with your database's username or password")
+			elif err.errno == errorcode.ER_BAD_DB_ERROR:
+				print("Database does not exist")
+			else:
+	 			print(err)
+			raw_input()
+
+	def login(self, username, pattern):
+		self.connect()
+		# This is unsafe
+		query = "SELECT name,email,record_time FROM users WHERE users.name = '%s' AND users.pattern = '%s';" % (username,pattern)
+		cursor = self.cnx.cursor()
+		cursor.execute(query)
+		result = []
+		for row in cursor:
+			result.append(row)
+		cursor.close()
+		self.cnx.close()
+		if len(result) > 0:
+			user_logged = {
+				'name': result[0][0],
+				'mail': result[0][1],
+				'wait': result[0][2]
+			}
+			return user_logged
+		else:
+			raise Exception("User not found")
